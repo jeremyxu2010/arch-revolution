78850463fbbfc938c9c66a3230d4641533d59e5a
Jeremy Xu <xinjiexu@tencent.com>
2019-06-05 11:27:05 +0800

    demo2在配置文件中指定apigateway的地址

--- demo2/frontend-service/src/main/java/personal/jeremyxu/archrevolutition/demo2/frontendservice/controller/FrontendController.java
+++ demo2/frontend-service/src/main/java/personal/jeremyxu/archrevolutition/demo2/frontendservice/controller/FrontendController.java
@@ -1,8 +1,6 @@
 package personal.jeremyxu.archrevolutition.demo2.frontendservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,22 +8,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class FrontendController {
 
-    @Autowired
-    DiscoveryClient discoveryClient;
+//    禁用DiscoveryClient
+//    @Autowired
+//    DiscoveryClient discoveryClient;
+
+    @Value("${app.apiGatewayUrl}")
+    String apiGatewayURL;
 
     @RequestMapping(value = "/api/injectClientConfig", method = RequestMethod.GET, produces = "text/javascript; charset=utf-8")
     public @ResponseBody String injectClientInfo(HttpServletRequest request, HttpSession session){
-        List<ServiceInstance> instances = discoveryClient.getInstances("apigateway-service");
-        if (instances.size() > 0){
-            ServiceInstance instance = instances.get(0);
-            String apiGatewayURL = instance.getUri().toString();
-            return String.format("window.API_GATEWAY_URL='%s';", apiGatewayURL);
-        }
-        return "if(window.console){window.console.log('not discovery api gateway address, use default.');}";
+//        List<ServiceInstance> instances = discoveryClient.getInstances("apigateway-service");
+//        if (instances.size() > 0){
+//            ServiceInstance instance = instances.get(0);
+//            String apiGatewayURL = instance.getUri().toString();
+//            return String.format("window.API_GATEWAY_URL='%s';", apiGatewayURL);
+//        }
+//        return "if(window.console){window.console.log('not discovery api gateway address, use default.');}";
+
+        // 直接输出API Gateway的访问地址
+        return String.format("window.API_GATEWAY_URL='%s';", apiGatewayURL);
     }
 }
